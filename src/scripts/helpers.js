6615de8fbe0d69568da233d99ef19c9211b30a73
Marvin Medeiros <marvin.medeiros52@gmail.com>
2017-07-11 16:30:33 -0300

    feature: Create a class with helpers

---
+++ src/scripts/helpers.js
@@ -0,0 +1,15 @@
+class Helpers {
+  secondsToHms(d) {
+    d = Number(d);
+
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+
+    return ('0' + h).slice(-2) + ":" + ('0' + m).slice(-2) + ":" + ('0' + s).slice(-2);
+  }
+}
+
+const helpers = new Helpers();
+
+export default helpers
