eff0c88bc445345d43abe1fa756a4572b42d4010
Marvin Medeiros <marvin.medeiros52@gmail.com>
2017-07-26 18:35:14 -0300

    refactor: change structure from home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 
+
+import feedcastApi from './../scripts/feedcastApi'
 import Episodes from './../components/home/episodes'
 import CategoriesSidebar from './../components/categoriesSidebar'
 import './../styles/home.sass'
 
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories : []
+    }
+  }
+
+
+  componentDidMount() {
+    feedcastApi
+    .getCategories()
+    .then( data => {
+        this.setState({categories : data.categories })
+    })
+  }
+
+
+
 
   render() {
     const {params} = this.props
     return (
       <div className="feedcast__home">
-        <CategoriesSidebar />
+        <CategoriesSidebar categories={this.state.categories} />
         <div className="feedcast__home-content-wrapper">
           <Episodes params={params}/>
         </div>
