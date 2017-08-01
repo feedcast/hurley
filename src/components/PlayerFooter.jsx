a79bc999b27eb9433d468ca168cc4c0734b2c5c0
Marvin Medeiros <marvin.medeiros52@gmail.com>
2017-08-01 18:51:53 -0300

    feature: initial implementation of the new player

--- src/components/PlayerFooter.jsx
+++ src/components/PlayerFooter.jsx
@@ -7,10 +7,19 @@ import './../styles/PlayerFooter.sass'
 class PlayerFooter extends Component {
   constructor(props) {
     super(props);
+
+    this.audioPlayer = document.createElement('AUDIO')
+
+    console.log(this.audioPlayer)
+    console.dir(this.audioPlayer)
     this.state = {
       episodes : [],
-      playingUuid : null
+      playingUuid : null,
+      isPaused: this.audioPlayer.paused,
+      canPlay: false,
+      loadedData: false
     }
+
 
     this.bindEvents()
   }
@@ -19,42 +28,80 @@ class PlayerFooter extends Component {
 
   bindEvents(){
     feedcastApi.on('play:episode', this.playEpisode.bind(this))
+
+    const t = 'AUDIOPLAYER'
+
+    //Player changing state
+    this.audioPlayer.onplay = e => {
+        console.log(`${t} - onplay:`, e);
+        this.setState({isPaused: false})
+    }
+    this.audioPlayer.onpause = e => {
+        console.log(`${t} - onpause:`, e);
+        this.setState({isPaused: true})
+    }
+    this.audioPlayer.oncanplay = e => {
+        console.log(`${t} - oncanplay:`, e);
+        this.setState({canPlay: true})
+    }
+    this.audioPlayer.onloadeddata = e => {
+        console.log(`${t} - onloadeddata:`, e);
+        this.setState({loadedData: true})
+    }
+
+    //ERROR HANDLING
+    const err = {
+      canPlay: false,
+      isPaused: true,
+      loadedData: false
+    }
+
+    this.audioPlayer.onabort = e => {
+        console.log(`${t} - onabort:`, e);
+        this.setState(err)
+    }
+    this.audioPlayer.onerror = e => {
+        console.log(`${t} - onerror:`, e);
+        this.setState(err)
+    }
+    // this.audioPlayer.onsuspend = e => {
+    //     console.log(`${t} - onsuspend:`, e);
+    //     this.setState(err)
+    // }
   }
 
 
 
   playEpisode(episode){
     let episodes = this.state.episodes
-    episodes.push(episode);
+    episodes[episode.uuid] = episode
 
-    this.setState({ episodes })
+    this.setState({ episodes, playingUuid: episode.uuid })
+
+
+    this.audioPlayer.src = episode.audio.url
+
+    this.audioPlayer.play()
   }
 
 
 
   webPlayer(){
 
-    let srcAudio = false,
-        titleEpisode = ' ',
-      { episodes } = this.state;
-
-    if(episodes.length > 0){
-      const lastEpisode = episodes[episodes.length - 1]
-      srcAudio = lastEpisode.audio.url
-      titleEpisode = lastEpisode.title
-    }
+    const {episodes, playingUuid} = this.state
+    const currentEpisode = episodes[playingUuid]
 
     return (
-      <div className={`feedcast__footer feedcast__footer--${episodes.length > 0 ? 'show':'hide'}`}>
+      <div className={`feedcast__footer feedcast__footer--${playingUuid !== null ? 'show':'hide'}`}>
         <div className="feedcast__playerFooter">
           <div className="feedcast__playerFooter-top">
-            <h5>{titleEpisode}</h5>
+            <h5></h5>
           </div>
           <div className="feedcast__playerFooter-bottom">
-            <audio
-              autoPlay={!helpers.mobilecheck()}
-              controls src={srcAudio}>
-            </audio>
+            <button
+              onClick={e=>{this.audioPlayer[`${this.state.isPaused?'play':'pause'}`]()}}>
+              <i className={`fa fa-${this.state.isPaused?'play':'pause'}`}></i>
+            </button>
           </div>
         </div>
       </div>
