292111309430a10df3341f0cfcba886a2f0b2f7f
Marvin Medeiros <marvin.medeiros52@gmail.com>
2017-08-05 23:17:04 -0300

    feature: initial implementation of a localization helper

---
+++ src/scripts/helpers/dictionary.js
@@ -0,0 +1,31 @@
+const dictionary = {
+
+  //English
+  ['en'] : {
+    default: true,
+    alias: 'English',
+    words: {
+      hello: 'Hello',
+      world: 'World'
+    }
+  },
+
+
+
+
+  //Portuguese
+  ['pt'] : {
+    default: false,
+    alias: 'Português',
+    words: {
+      hello: 'Olá',
+      world: 'Mundo'
+    }
+  }
+
+
+}
+
+
+
+export default dictionary
