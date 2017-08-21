59a9d761acbd265d5c4aadaa4bb0a3f00a9d0685
Marvin Medeiros <marvin.medeiros52@gmail.com>
2017-08-21 01:44:04 -0300

    feature: add disqus comments to the episode page

--- src/pages/episode/index.js
+++ src/pages/episode/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import feedcastApi from 'feedcast-client'
+import ReactDisqusComments from 'react-disqus-comments';
 
 
 export default class Episode extends Component {
@@ -25,20 +26,41 @@ export default class Episode extends Component {
         this.setState(data)
       })
       .catch( e => {
-        alert('Podcast não encontrado ou algum erro: ' + e.message)
+        console.log(e.message)
       })
   }
 
+
+  handleNewComment(comment) {
+    console.log(comment.text);
+  }
+
+
   render() {
+    const { slug, episode_slug} = this.props.match.params
+
     const {
       title,
       summary,
+      description,
+      channel,
     } = this.state
 
     return (
-      <div className="feedcast__section">
-        <h3>{title}</h3>
-        <p>{summary}</p>
+      <div>
+        <div className="feedcast__section">
+          <h3>{title}</h3>
+          <p>{summary.length > 0 ? summary : description }</p>
+        </div>
+        <div className="feedcast__section">
+          <ReactDisqusComments
+            shortname="feedcast"
+            identifier={`${slug}/${episode_slug}`}
+            title={title}
+            url={window.location.href}
+            category_id={ channel.slug }
+            onNewComment={this.handleNewComment}/>
+        </div>
       </div>
     )
   }
