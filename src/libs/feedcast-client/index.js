247410538f1e27c9b8ebbaebfdbe375a54a45cb4
Marvin Medeiros <marvin.medeiros52@gmail.com>
2017-09-18 00:49:21 -0300

    feature: initial implementation of localstorage cache (WIP)

--- src/libs/feedcast-client/index.js
+++ src/libs/feedcast-client/index.js
@@ -2,29 +2,126 @@ const API_URL = process.env.REACT_APP_API_HOST;
 
 class FeedcastApi {
 
-  fetch({ url = null }){
+  fetch({ url = null, cache = false, cacheTime = 60*60*1000 }){
     return new Promise((complete, reject) => {
-      const req = new XMLHttpRequest();
+      let fromcache = false
 
-      req.open('GET', url, true);
+      if(cache === true) {
+        let fromcache = this.storage('get', { url, cache, cacheTime })
+        console.log(fromcache)
+        if(fromcache !== false){
+          complete(fromcache);
+        }
+      }
 
-      req.onload = () => {
-        let result = JSON.parse(req.response)
-        result.total = req.getResponseHeader('total')
-        complete(result);
-      };
+      if(fromcache === false){
+        const req = new XMLHttpRequest();
 
-      req.onerror = () => {
-        reject(req.statusText);
-      };
+        req.open('GET', url, true);
 
-      req.send();
+        req.onload = () => {
+          let result = JSON.parse(req.response)
+          result.total = req.getResponseHeader('total')
+          if(cache === true) this.storage('set', { url, cache, cacheTime, response: result });
+          complete(result);
+        };
+
+        req.onerror = () => {
+          reject(req.statusText);
+        };
+
+        req.send();
+      }
+
     })
   }
 
+  storage(action, payload){
+
+      const {
+        url,
+        cache,
+        cacheTime,
+        response = {}
+      } = payload
+
+      const key = btoa(Date.now())
+
+      switch(action){
+        case 'set':
+          return this.setCacheList({
+            config: { key, cacheTime, timestamp: Date.now(), url },
+            response
+          });
+        break;
+        case 'get':
+          return this.getRequestFromCache({ url, cacheTime})
+        break;
+      }
+
+    return false
+  }
+
+  getRequestFromCache({ url, cacheTime }){
+
+    let ls = localStorage
+
+    if(ls.getItem('cachelist') !== null){
+      const cachelist = JSON.parse(ls.getItem('cachelist'))
+
+      let fetchCache = ( u, c ) => {
+        for(let i in c)
+          if(c[i].url === u)
+            return c[i]
+          return false
+      }
+
+      const res = fetchCache(url, cachelist)
+
+      if(res !== false){
+        let cache = ls.getItem(res.key)
+        if(cache !== null){
+          cache = JSON.parse(cache)
+          if( ( Date.now() - cache.timestamp ) < cacheTime ){
+            return cache
+          } else {
+            ls.removeItem(res.key)
+          }
+        }
+      }
+
+    }
+
+    return false
+  }
+
+
+  setCacheList({ config = null, response = {} }){
+    let ls = localStorage
+    if(ls.getItem('cachelist') === null){
+      ls.setItem('cachelist', '[]')
+    }
+
+    if(config !== null
+      && typeof config === 'object'
+      && ls.getItem('cachelist') !== null){
+
+      let cachelist = JSON.parse(ls.getItem('cachelist'))
+
+      if(Array.isArray(cachelist)){
+        cachelist.push(config)
+        ls.setItem('cachelist', JSON.stringify(cachelist))
+        ls.setItem(config.key, JSON.stringify(response))
+        console.info('new item cached on local storage')
+      }
+
+    }
+  }
+
+
   getChannels({page = 1, per_page = 24}){
     const url = `${API_URL}/channels?page=${page}&per_page=${per_page}`
-    return this.fetch({ url })
+    return this.fetch({ url, cache: true })
   }
 
   getEpisodesByCategory({page = 1, per_page = 100, slug = ''}){
