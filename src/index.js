924d8dc099733f82942651ca8d8238d365ca63f7
Cristian Oliveira <contato@cristianoliveira.com.br>
2018-01-02 15:52:57 +0100

    refactor: Change jsx to js

--- src/index.js
+++ src/index.js
@@ -23,6 +23,8 @@ import Episode from './pages/episode';
 import Queue from './pages/queue';
 import store from './store';
 
+import 'app/i18n';
+
 ReactGA.initialize(process.env.REACT_APP_GA);
 
 rollbarClient({
@@ -36,10 +38,10 @@ rollbarClient({
 
 
 const logPageView = () => {
+    console.log("Log", { page: window.location.pathname + window.location.search })
     window.scrollTo(0, 0);
     ReactGA.set({ page: window.location.pathname + window.location.search });
     ReactGA.pageview(window.location.pathname + window.location.search);
-    return null;
 };
 
 const NoMatch = ({ location }) => (
@@ -52,17 +54,16 @@ function AppRouter() {
   return (
   <Provider store={store}>
     <Router>
-      <Page>
-        <Route path="/" component={logPageView}></Route>
+      <Page onUpdate={ logPageView } >
         <Switch>
-          <Route exact path="/" component={Home}></Route>
-          <Route path="/queue/" component={Queue}></Route>
-          <Route path="/channels/:page?" component={ChannelList}></Route>
-          <Route path="/category/:slug" component={Category}></Route>
-          <Route path="/episodes/:page?" component={EpisodesList}></Route>
-          <Route exact path="/:slug" component={Channel}></Route>
-          <Route path="/:slug/episodes/:page(\d+)?" component={Channel}></Route>
-          <Route path="/:slug/:episode_slug" component={Episode}></Route>
+          <Route path="/" component={Home} />
+          <Route path="/queue/" component={Queue} />
+          <Route path="/channels/:page?" component={ChannelList} />
+          <Route path="/category/:slug" component={Category} />
+          <Route path="/episodes/:page?" component={EpisodesList} />
+          <Route exact path="/:slug" component={Channel} />
+          <Route path="/:slug/episodes/:page(\d+)?" component={Channel} />
+          <Route path="/:slug/:episode_slug" component={Episode} />
           <Route component={NoMatch}/>
         </Switch>
       </Page>
